3a4e406ed9805bd84f56683bd86832f01f573b46
Hadi Yousufi <reach.hadiy@gmail.com>
2017-07-21 22:43:08 -0500

    server works

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.set("view engine", "ejs");
+
+app.get('/', function(req, res){
+    res.render('landing');
+})
+
+
+app.listen(process.env.PORT || 8080, function(){
+    console.log('NICE! The YELPCAMP SERVER has started.');
+});
