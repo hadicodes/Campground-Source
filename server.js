18eae4dd4313af7c11532bad598ca43fd4875303
Hadi Yousufi <reach.hadiy@gmail.com>
2017-07-22 23:59:12 -0500

    server works

--- server.js
+++ server.js
@@ -8,6 +8,13 @@ app.get('/', function (req, res) {
     res.render('landing');
 })
 
+app.get('/campgrounds', function(){
+    var campgrounds = [
+        {name: "Salmon Creek", image: ""}
+        {name: "Granite Hill", image: ""}
+    ]
+})
+
 
 app.listen(process.env.PORT || 8080, function () {
     console.log('NICE! The YELPCAMP SERVER has started.');
