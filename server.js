e8f01b537266f0de175b3ca04511a51d357d1090
Hadi Yousufi <reach.hadiy@gmail.com>
2017-07-30 23:40:23 -0500

    adds campgrounds route

--- server.js
+++ server.js
@@ -8,19 +8,20 @@ app.get('/', function (req, res) {
     res.render('landing');
 })
 
-app.get('/campgrounds', function(){
+app.get('/campgrounds', function(req, res){
     var campgrounds = [
         {name: "Salmon Creek", image: "http://www.photosforclass.com/download/7626464792"},
         {name: "Granite Hill", image: "https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg"},
         {name: "Mountain Goat's Rest", image: "http://www.photosforclass.com/download/5641024448"}
     ]
+res.render('campgrounds');
 });
 
-res.render('campgrounds');
+
 
 
 app.listen(process.env.PORT || 8080, function () {
-    console.log('NICE! The YELPCAMP SERVER has started.');
+    console.log('NICE! YELPCAMP SERVER has startedon PORT 8080.');
 });
 
 
