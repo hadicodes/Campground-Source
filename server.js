173e638e82768e0cc3defaf30d27df918aa09b3b
Hadi Yousufi <reach.hadiy@gmail.com>
2017-08-10 23:54:30 -0500

    adds post route to campgrounds

--- server.js
+++ server.js
@@ -5,10 +5,12 @@ const bodyParser = require("body-parser");
 // middleware
 app.set("view engine", "ejs");
 
+// Landing page route
 app.get('/', function (req, res) {
     res.render('landing');
 })
 
+// Show all campgrounds
 app.get('/campgrounds', function(req, res){
     var campgrounds = [
         {name: "Salmon Creek", image: "http://www.photosforclass.com/download/7626464792"},
@@ -18,9 +20,18 @@ app.get('/campgrounds', function(req, res){
 res.render('campgrounds', {campgrounds: campgrounds} );
 });
 
+// Create a new  campground
+app.post('/campgrounds', function(req, res){
+    res.send('you hit post route');
+    // get data from form and redirect to campgrounds array
+
+
+    // redirect back to campgrounds page
+});
 
 
 
+// Server Port Listener
 app.listen(process.env.PORT || 8080, function () {
     console.log('SUCCESS! visit PORT 8080');
 });
