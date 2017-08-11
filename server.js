33d8cc886b4e710b26c6964ed4980153d85e2579
Hadi Yousufi <reach.hadiy@gmail.com>
2017-08-11 01:01:26 -0500

    add new campground working currently with an array to store data(campgrounds)

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const app = express();
 const bodyParser = require("body-parser");
 
 // middleware
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.set("view engine", "ejs");
 
 // Landing page route
@@ -11,27 +13,45 @@ app.get('/', function (req, res) {
     res.render('landing');
 })
 
+
+var campgrounds = [{
+        name: "Salmon Creek",
+        image: "http://www.photosforclass.com/download/7626464792"
+    },
+    {
+        name: "Granite Hill",
+        image: "https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg"
+    },
+    {
+        name: "Mountain Goat's Rest",
+        image: "http://www.photosforclass.com/download/5641024448"
+    }
+]
 // Show all campgrounds
-app.get('/campgrounds', function(req, res){
-    var campgrounds = [
-        {name: "Salmon Creek", image: "http://www.photosforclass.com/download/7626464792"},
-        {name: "Granite Hill", image: "https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg"},
-        {name: "Mountain Goat's Rest", image: "http://www.photosforclass.com/download/5641024448"}
-    ]
-res.render('campgrounds', {campgrounds: campgrounds} );
+app.get('/campgrounds', function (req, res) {
+    res.render('campgrounds', {
+        campgrounds: campgrounds
+    });
 });
 
 
 // Show new campground form
-app.get('/campgrounds/new', function(req, res){
+app.get('/campgrounds/new', function (req, res) {
     res.render('new.ejs');
 });
 
 // Create a new  campground
-app.post('/campgrounds', function(req, res){
-    res.send('you hit da post route');
+app.post('/campgrounds', function (req, res) {
     // get data from form and redirect to campgrounds array
+    let name = req.body.name;
+    let image = req.body.image;
+    let newCampground = {
+        name: name,
+        image: image
+    }
+    campgrounds.push(newCampground);
     // redirect back to campgrounds page
+    res.redirect('/campgrounds');
 });
 
 
@@ -48,5 +68,3 @@ app.post('/campgrounds', function(req, res){
 app.listen(process.env.PORT || 8080, function () {
     console.log('SUCCESS! visit PORT 8080');
 });
-
-
