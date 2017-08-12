ee4717d6a930d2729e0c19b9cc7184c575e7c02a
Hadi Yousufi <reach.hadiy@gmail.com>
2017-08-11 23:13:31 -0500

    adds list of all campgrounds into a grid of responsive thumbnails

--- server.js
+++ server.js
@@ -34,7 +34,32 @@ var campgrounds = [{
     {
         name: "Mountain Goat's Rest",
         image: "http://www.photosforclass.com/download/5641024448"
+    },
+    {
+        name: "Salmon Creek",
+        image: "http://www.photosforclass.com/download/7626464792"
+    },
+    {
+        name: "Granite Hill",
+        image: "https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg"
+    },
+    {
+        name: "Mountain Goat's Rest",
+        image: "http://www.photosforclass.com/download/5641024448"
+    },
+    {
+        name: "Salmon Creek",
+        image: "http://www.photosforclass.com/download/7626464792"
+    },
+    {
+        name: "Granite Hill",
+        image: "https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg"
+    },
+    {
+        name: "Mountain Goat's Rest",
+        image: "http://www.photosforclass.com/download/5641024448"
     }
+
 ]
 // Show all campgrounds
 app.get('/campgrounds', function (req, res) {
