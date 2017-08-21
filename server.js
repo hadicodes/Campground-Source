974de3a9872cafdf336190b010221945ae8eac83
Hadi Yousufi <reach.hadiy@gmail.com>
2017-08-20 23:58:00 -0500

    adds show more info page. Campground.description not displaying

--- server.js
+++ server.js
@@ -31,17 +31,17 @@ const campgroundSchema = new mongoose.Schema({
 const Campground = mongoose.model("Campground", campgroundSchema);
 
 // Create a new Campground and add it to our db
-Campground.create({
-    name: "FireSide Rock",
-    image: "https://farm1.staticflickr.com/7/5954480_34a881115f.jpg",
-    descripton: "This campground is known to have regular forest fires. Tread carefully, you have been Warned!"
-}, function (err, newlyCreated) {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log("success");
-    }
-});
+// Campground.create({
+//     name: "FireSide Rock",
+//     image: "https://farm1.staticflickr.com/7/5954480_34a881115f.jpg",
+//     descripton: "This campground is known to have regular forest fires. Tread carefully, you have been Warned!"
+// }, function (err, newlyCreated) {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log("success");
+//     }
+// });
 
 
 
