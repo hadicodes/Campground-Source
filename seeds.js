80914a19b4c6bf57cffcdee4bf1cfecce3a7b027
Hadi Yousufi <reach.hadiy@gmail.com>
2017-09-17 23:59:17 -0500

    adds in a few campgrounds

--- seeds.js
+++ seeds.js
@@ -1,13 +1,18 @@
 const mongoose = require("mongoose"),
     Campground = require("./models/campground");
 
-    
-    
-    function seedDB(){
-        Campground.remove({}, function (err) {
-            if (err) {
-                console.log(err);
-            }
-            console.log('removed campgrounds!');
-        });
+
+
+function seedDB() {
+    // Removes all campgrounds
+    Campground.remove({}, function (err) {
+        if (err) {
+            console.log(err);
+        }
+        console.log('removed campgrounds!');
+    });
+    // Adds in a few campgrounds
+    Cam
 }
+
+module.exports = seedDB;
