83f76f9d4880d64309ba2b03b6357c391fa3f38e
Hadi Yousufi <reach.hadiy@gmail.com>
2017-09-18 00:06:18 -0500

    adds in a few campgrounds

--- seeds.js
+++ seeds.js
@@ -1,6 +1,22 @@
 const mongoose = require("mongoose"),
     Campground = require("./models/campground");
 
+var data = [{
+        name: "Cloud's Rest",
+        image: "https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg",
+        description: "A nice campground with soft ground near the clouds."
+    },
+    {
+        name: "Desert Mesa",
+        image: "https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg",
+        description: "Dry barren desert surrounds this campground."
+    },
+    {
+        name: "Cloud's Rest",
+        image: "https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg",
+        description: "A nice campground with soft ground near the clouds."
+    }
+]
 
 
 function seedDB() {
