8499a9ef892081a0c95ca5c85daa7f416130dd26
Hadi Yousufi <reach.hadiy@gmail.com>
2017-09-22 00:15:41 -0500

    adds new comment functionality

--- server.js
+++ server.js
@@ -117,6 +117,40 @@ app.get("/campgrounds/:id/comments/new", function (req, res) {
 
 });
 
+
+
+app.post('/campgrounds/:id/comments', function(req, res){
+    // Lookup campground using id
+    Campground.findById(req.params.id, function(err, campground){
+        if (err) {
+                console.log("You have an ERROR!!!");
+                res.redirect("/campgrounds");
+        } else {
+            console.log(req.body.comment);
+            Comment.create(req.body.comment, function(err, comment){
+                if (err) {
+                        console.log("You have an ERROR!!!");
+                } else {
+                    campground.comments.push(comment);
+                    campground.save();
+                    res.redirect("/campgrounds/" + campground._id);
+                }
+            });
+        }
+    });
+    // create comment
+
+    // connect new comment to campground
+
+    // redirect to campground show page
+});
+
+
+
+
+
+
+
 // Server Port Listener
 app.listen(process.env.PORT || 8080, function () {
     console.log('SUCCESS! visit PORT 8080');
