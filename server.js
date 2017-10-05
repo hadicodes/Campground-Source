19e10abe7d7396a88ea0203863f634db707cbc61
Hadi Yousufi <reach.hadiy@gmail.com>
2017-10-05 00:29:53 -0500

    adds sighup authentication for new registersç

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ seedDB();
 app.use(require("express-session")({
     secret: "Rusty is the cutest dog",
     resave: false,
-    saveUninitialized: false    
+    saveUninitialized: false
 }));
 // In an Express-based application, passport.initialize() middleware is required to initialize Passport. If your application uses persistent login sessions, passport.session() middleware must also be used.
 app.use(passport.initialize());
@@ -172,13 +172,24 @@ app.post('/campgrounds/:id/comments', function (req, res) {
 //======================================================
 //AUTH ROUTES
 //======================================================
-app.get('/register', function (req, res) {
-    res.render("register");
-});
 
-
-
-
+// show register form
+app.get("/register", function(req, res){
+    res.render("register"); 
+ });
+ //handle sign up logic
+ app.post("/register", function(req, res){
+     var newUser = new User({username: req.body.username});
+     User.register(newUser, req.body.password, function(err, user){
+         if(err){
+             console.log(err);
+             return res.render("register");
+         }
+         passport.authenticate("local")(req, res, function(){
+            res.redirect("/campgrounds"); 
+         });
+     });
+ });
 
 
 
@@ -186,5 +197,3 @@ app.get('/register', function (req, res) {
 app.listen(process.env.PORT || 3000, function () {
     console.log('SUCCESS! visit PORT 8080');
 });
-
-
